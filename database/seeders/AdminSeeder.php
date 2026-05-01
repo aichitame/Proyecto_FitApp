9d5e38c640e1c4b32ced45d10324815385f6285c
aichitame <aixalrk@gmail.com>
2026-05-02 00:25:53 +0200

    Make demo users seedable in production

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
@@ -14,20 +13,22 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //crear el administrador
-        User::create([
-            'name' => 'Admin FitApp',
-            'email' => 'admin@fitapp.com',
-            'password' => Hash::make('Admin123!'), //cumple con el RF-01
-            'role' => 'admin',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@fitapp.com'],
+            [
+                'name' => 'Admin FitApp',
+                'password' => Hash::make('Admin123!'),
+                'role' => 'admin',
+            ]
+        );
 
-        //crear un cliente de prueba
-        User::create([
-            'name' => 'Cliente Aixa',
-            'email' => 'client@test.com',
-            'password' => Hash::make('Cliente123!'),
-            'role' => 'client',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'client@test.com'],
+            [
+                'name' => 'Cliente Aixa',
+                'password' => Hash::make('Cliente123!'),
+                'role' => 'client',
+            ]
+        );
     }
 }
